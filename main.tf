a5de5e31a3c606b673e27e7c4b0eb194accd21b8
Sailakshith6 <117815467+Sailakshith6@users.noreply.github.com>
2024-10-29 13:50:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-# Specify required providers
+# We strongly recommend using the required_providers block to set the  
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "=2.75.0"
     }
   }
@@ -10,15 +11,18 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
 
-# Data sources and resources for network configuration
 data "azurerm_resource_group" "hcmxexample" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
 }
 
 resource "azurerm_public_ip" "hcmxexample" {
@@ -50,12 +54,11 @@ resource "azurerm_network_interface" "hcmxexample" {
     subnet_id                     = data.azurerm_subnet.hcmxexample.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.hcmxexample.id
+                 }
   }
-}
-
-# Define Linux VM with optional custom image
-resource "azurerm_linux_virtual_machine" "hcmxexample" {
-  count               = var.os_type == "linux" ? 1 : 0
+  
+ resource "azurerm_linux_virtual_machine" "hcmxexample" {
+  count = var.os_type=="linux" ? 1 : 0
   name                = var.vm_name
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
   location            = var.location
@@ -66,30 +69,21 @@ resource "azurerm_linux_virtual_machine" "hcmxexample" {
   network_interface_ids = [
     azurerm_network_interface.hcmxexample.id,
   ]
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.type_of_storage
   }
-
-  dynamic "source_image_reference" {
-    for_each = var.image_id != "" ? [1] : []
-    content {
-      id = var.image_id
-    }
-  }
-
+   
   source_image_reference {
-    publisher = var.image_id == "" ? var.publisher : null
-    offer     = var.image_id == "" ? var.offer : null
-    sku       = var.image_id == "" ? var.sku : null
-    version   = var.image_id == "" ? var.os_version : null
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
   }
 }
 
-# Define Windows VM with optional custom image
 resource "azurerm_windows_virtual_machine" "hcmxexample" {
-  count               = var.os_type == "windows" ? 1 : 0
+  count = var.os_type=="windows" ? 1 : 0
   name                = var.vm_name
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
   location            = var.location
@@ -99,57 +93,47 @@ resource "azurerm_windows_virtual_machine" "hcmxexample" {
   network_interface_ids = [
     azurerm_network_interface.hcmxexample.id,
   ]
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.type_of_storage
   }
-
-  dynamic "source_image_reference" {
-    for_each = var.image_id != "" ? [1] : []
-    content {
-      id = var.image_id
-    }
-  }
-
+   
   source_image_reference {
-    publisher = var.image_id == "" ? var.publisher : null
-    offer     = var.image_id == "" ? var.offer : null
-    sku       = var.image_id == "" ? var.sku : null
-    version   = var.image_id == "" ? var.os_version : null
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
   }
 }
 
-# Managed Disk
 resource "azurerm_managed_disk" "hcmxexample" {
   name                 = "${var.vm_name}-disk"
   location             = var.location
-  resource_group_name  = data.azurerm_resource_group.hcmxexample.name
+  resource_group_name = data.azurerm_resource_group.hcmxexample.name
   storage_account_type = var.type_of_storage
   create_option        = "Empty"
   disk_size_gb         = var.disk_size
 }
 
-# Data Disk Attachment
 resource "azurerm_virtual_machine_data_disk_attachment" "hcmxexample" {
   managed_disk_id    = azurerm_managed_disk.hcmxexample.id
-  virtual_machine_id = var.os_type == "linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
-  lun                = 10
+  virtual_machine_id = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
+  lun                = "10"
   caching            = "ReadWrite"
 }
 
-# Data sources for Public IP and Network Interface
 data "azurerm_public_ip" "hcmxexample" {
   name                = var.vm_name
-  resource_group_name = data.azurerm_resource_group.hcmxexample.name
+  resource_group_name = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].resource_group_name : azurerm_windows_virtual_machine.hcmxexample[0].resource_group_name
+  
 }
 
 data "azurerm_network_interface" "hcmxexample" {
   name                = var.vm_name
-  resource_group_name = data.azurerm_resource_group.hcmxexample.name
+  resource_group_name = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].resource_group_name : azurerm_windows_virtual_machine.hcmxexample[0].resource_group_name
+
 }
 
-# Outputs
 output "public_ip_address" {
   value = data.azurerm_public_ip.hcmxexample.ip_address
 }
@@ -167,11 +151,11 @@ output "primary_dns_name" {
 }
 
 output "virtual_machine_id" {
-  value = var.os_type == "linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
+  value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].virtual_machine_id : azurerm_windows_virtual_machine.hcmxexample[0].virtual_machine_id
 }
 
 output "cloud_instance_id" {
-  value = var.os_type == "linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
+  value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
 }
 
 output "data_disk_name" {
